7fc08b105e9e1b7a9676a2a80c24cefb97c154b4
inhooinu <59385391+inhooinu@users.noreply.github.com>
2024-11-23 17:20:27 +0900

    Title: 달에서 무게 구하기, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241123/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int w = 13;
+        double r = 0.165;
+
+        System.out.printf("%d * %6f = %6f", w, r, w*r); 
+    }
+}
