938afb1ccb15b779a2015f3dded3503c9efd2b43
inhooinu <59385391+inhooinu@users.noreply.github.com>
2024-12-16 19:26:12 +0900

    Title: 일 수 구하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241216/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if (n==2) System.out.print(28);
+        else if (n==1 || n==3 || n==5 || n==7 || n==8 || n==10 || n==12) System.out.print(31);
+        else System.out.print(30);
+    }
+}
