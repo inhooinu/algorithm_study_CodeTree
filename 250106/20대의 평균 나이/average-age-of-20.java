6beb5b4b4546e6ed140468f8eaa8a3c57a05dac0
inhooinu <59385391+inhooinu@users.noreply.github.com>
2025-01-06 13:00:59 +0900

    Title: 20대의 평균 나이, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250106/20대의 평균 나이/average-age-of-20.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int age;
+        int cnt = 0;
+        int sum = 0;
+        while (true) {
+            age = sc.nextInt();
+            
+            if (age<20 || age>=30) {
+                System.out.printf("%.2f", (double)sum/cnt);
+                break;
+            }
+
+            cnt++;
+            sum+=age;
+        }
+    }
+}
