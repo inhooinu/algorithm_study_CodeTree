e189722ed62ef680b969303047a505b4fe8a678b
inhooinu <59385391+inhooinu@users.noreply.github.com>
2025-01-10 15:40:04 +0900

    Title: 별 그리기, Time: 182ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 250110/별 그리기/star-drawing.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int blanks = n-1;
+        int stars = 1;
+
+        for (int i=0; i<(2*n-1); i++) {
+            for (int j=0; j<blanks; j++) {
+                System.out.print(" ");
+            }
+            for (int j=0; j<stars; j++) {
+                System.out.print("*");
+            }
+
+            if (i<n-1) {
+                blanks--;
+                stars+=2;
+            } else {
+                blanks++;
+                stars-=2;
+            }
+            
+            System.out.println();
+        }
+    }
+}
