4eae436e92d50b6f6ff749f67ab02de9fc88a3cc
inhooinu <59385391+inhooinu@users.noreply.github.com>
2025-01-16 16:35:44 +0900

    Title: 숫자 차례로 출력하기, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250116/숫자 차례로 출력하기/to-output-numerically.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+# Write your code here!
+def printAsc(n):
+    if (n==0): return
+    
+    printAsc(n-1)
+    print(n, end=" ")
+
+def printDesc(n):
+    if (n==0): return
+
+    print(n, end=" ")
+    printDesc(n-1)
+
+printAsc(n)
+print()
+printDesc(n)
