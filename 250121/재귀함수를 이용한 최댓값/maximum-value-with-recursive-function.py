2dddc05f271007dca4438585bf415292f52e5b0c
inhooinu <59385391+inhooinu@users.noreply.github.com>
2025-01-21 19:01:10 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 5036ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 250121/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 250121/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -2,10 +2,7 @@ def find_max(arr):
     if len(arr)==1:
         return arr[0]
     
-    if arr[0] > find_max(arr[1:]):
-        return arr[0]
-    else:
-        return find_max(arr[1:])
+    return arr[0] if arr[0] > find_max(arr[1:]) else find_max(arr[1:])
 
 n = int(input())
 nums = list(map(int, input().split()))
