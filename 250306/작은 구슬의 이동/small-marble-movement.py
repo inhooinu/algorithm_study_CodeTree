c7b97e2e33cafd4dd82dcaeedb96c9eb98a90ff7
inhooinu <59385391+inhooinu@users.noreply.github.com>
2025-03-06 20:58:54 +0900

    Title: 작은 구슬의 이동, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250306/작은 구슬의 이동/small-marble-movement.py
+++ 250306/작은 구슬의 이동/small-marble-movement.py
@@ -4,13 +4,13 @@ R, C, D = input().split()
 R = int(R)
 C = int(C)
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+dx = [0, 1, -1, 0]
+dy = [1, 0, 0, -1]
 
 mapper = {
     "U": 2,
-    "D": 0,
-    "R": 1,
+    "D": 1,
+    "R": 0,
     "L": 3
 }
 
@@ -20,11 +20,9 @@ def in_range(R, C):
     return R>=1 and R<=N and C>=1 and C<=N
 
 for _ in range(T):
-    R, C = R + dx[dir_num], C + dy[dir_num]
-
-    if not in_range(R, C):
-        R, C = R - dx[dir_num], C - dy[dir_num]
-        dir_num = abs(2-dir_num)
-
+    if not in_range(R + dx[dir_num], C + dy[dir_num]):
+        dir_num = 3-dir_num
+    else:
+        R, C = R + dx[dir_num], C + dy[dir_num]
 
 print(R, C)
