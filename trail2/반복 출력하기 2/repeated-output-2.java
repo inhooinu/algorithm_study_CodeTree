336f3a8e79718e5d5eb615357d83e2ccb007fcef
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-06-05 17:56:40 +0900

    [Passed] 반복 출력하기 2 (128ms, 10MB)

---
+++ trail2/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        printHelloWorld(n);
+    }
+
+    public static void printHelloWorld(int n) {
+        if (n==0) {
+            return;
+        }
+
+        printHelloWorld(n-1);
+        System.out.println("HelloWorld");
+    }
+}
