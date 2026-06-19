2b7f8587757ea27af8b4bf6af230d0dfe74868f3
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-06-19 11:16:31 +0900

    [Passed] 2개씩 그룹짓기 (201ms, 13MB)

---
+++ trail2/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[2*n];
+
+        for (int i=0; i<2*n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        Arrays.sort(nums);
+        
+        int maxSum = 0;
+        for (int i=0; i<n; i++) {
+            maxSum = Math.max(maxSum, nums[i]+nums[2*n-1-i]);
+        }
+        System.out.println(maxSum);
+    }
+}
