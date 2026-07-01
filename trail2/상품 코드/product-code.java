0097c34e5e807d34ade14db91a079e0f7fd0d7a5
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-07-01 12:13:33 +0900

    [Passed] 상품 코드 (129ms, 10MB)

---
+++ trail2/상품 코드/product-code.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Product {
+    String name;
+    int code;
+
+    public Product(String name, int code) {
+        this.name=name;
+        this.code=code;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Product p1 = new Product("codetree", 50);
+        System.out.println("product "+p1.code+" is "+p1.name);
+        
+        String name = sc.next();
+        int code = sc.nextInt();
+        Product p2 = new Product(name, code);
+        System.out.println("product "+p2.code+" is "+p2.name);
+    }
+}
