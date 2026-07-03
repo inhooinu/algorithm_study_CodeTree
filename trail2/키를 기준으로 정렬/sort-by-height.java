9920d240d25f7c2a7313e21b96100647a81e3c6e
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-07-03 16:33:42 +0900

    [Passed] 키를 기준으로 정렬 (133ms, 10MB)

---
+++ trail2/키를 기준으로 정렬/sort-by-height.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Person implements Comparable<Person> {
+    String name;
+    int height;
+    int weight;
+
+    public Person(String name, int height, int weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public int compareTo(Person o) {
+        return Integer.compare(this.height, o.height);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Person[] people = new Person[n];
+        for (int i=0; i<n; i++) {
+            String name = sc.next();
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+
+            people[i] = new Person(name, height, weight);
+        }
+
+        Arrays.sort(people);
+
+        for (int i=0; i<n; i++) {
+            System.out.println(people[i].name+" "+people[i].height+" "+people[i].weight);
+        }
+    }
+}
