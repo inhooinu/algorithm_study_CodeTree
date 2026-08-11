a4065fb864ec4a3ebb132cea86bbe7f55deab87f
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-08-11 09:19:00 +0900

    [Passed] 키, 몸무게를 기준으로 정렬 (129ms, 10MB)

---
+++ trail2/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+class Person implements Comparable<Person> {
+    String name;
+    int height;
+    int weight;
+
+    Person(String name, int height, int weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public int compareTo(Person p) {
+        if (this.height != p.height) {
+            return Integer.compare(this.height, p.height);
+        }
+        return Integer.compare(p.weight, this.weight);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Person[] people = new Person[n];
+
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+            
+            people[i] = new Person(name, height, weight);
+        }
+
+        Arrays.sort(people);
+        for (int i=0; i<n; i++) {
+            System.out.println(people[i].name + " " + people[i].height + " " + people[i].weight);
+        }
+    }
+}
