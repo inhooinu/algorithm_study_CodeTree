4b1f2f24bd7536ce946e85188199e853821bd1e6
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-08-11 13:48:03 +0900

    [Passed] Time to Time (146ms, 10MB)

---
+++ trail2/Time to Time/time-to-time.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int result = (c*60+d) - (a*60+b);
+        System.out.println(result);
+    }
+}
